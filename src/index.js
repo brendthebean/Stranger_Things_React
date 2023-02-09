5fd9e0919aff3e96728044f893037296c1e470be
Brenden Smith <brendthebean@gmail.com>
2023-02-09 10:04:21 -0600

    posts rendered and fetched from api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { createRoot } from 'react-dom/client';
+import RenderPosts from "./renderPosts";
+import Header from "./header.js";
 
 const appElement = document.getElementById("root");
 
 const root = createRoot(appElement);
 
+
 const App = () => {
+    const [userPosts, setUserPosts] = useState([]);
+    console.log('posts: ', userPosts);
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch('https://strangers-things.herokuapp.com/api/2211-ftb-et-web-am/posts');
+            const data = await response.json();
+            setUserPosts(data.data.posts);            
+        }
+        fetchPosts();
+    }, [])
+    
     return <>
-        <h1>testing</h1>
+        <Header />
+        <RenderPosts userPosts={userPosts}/>       
     </>
 }
 
---
+++ src/renderPosts.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const RenderPosts = (props) => {
+
+    return (
+        <>
+        {props.userPosts.map((post) => {
+            return <div id="singlePost" key={post.id}>
+                <h2>{post.title}</h2>
+                <span>
+                    <h4>{post.price}</h4>
+                    <h5>{post.author.username}</h5>
+                </span>
+            </div>
+        })}
+        </>
+    )
+}
+
+export default RenderPosts;
